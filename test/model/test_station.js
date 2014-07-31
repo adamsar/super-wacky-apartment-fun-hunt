df854a533bf83ae16addbb3441906532bf5cab88
adamsar <adamsar@gmail.com>
2014-07-31 17:55:58 +0900

    Test stub

---
+++ test/model/test_station.js
@@ -0,0 +1,11 @@
+/**
+ * Tests for Station model
+ */
+
+var chai = require('chai'),
+    expect = chai.expect,
+    should = chai.should;
+
+describe('The station model', function () {
+
+}):
--- test/test_spec.js
+++
@@ -1,9 +0,0 @@
-var chai = require('chai'),
-    expect = chai.expect,
-    should = chai.should;
-
-describe('Preliminary test', function () {
-  it("should pass", function () {
-    chai.expect(true).to.equal(true);
-  });
-});
